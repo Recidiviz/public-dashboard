542643424fe11825483e2d1c472b65ca97a08f77
Justin Spradlin <jspradlin@gmail.com>
2020-07-23 07:57:50 -0400

    Probation Page: Free Through Recovery Program

--- src/page-probation/PageProbation.js
+++ src/page-probation/PageProbation.js
@@ -1,9 +1,11 @@
 import React from "react";
 import supervisionPopulationData from "../assets/test_data/US_ND_supervision_population_by_district_by_demographics.json";
+import supervisionProgramParticipationByRegion from "../assets/test_data/US_ND_active_program_participation_by_region.json";
 import judicialDistricts from "../assets/test_data/US_ND_judicial_districts.json";
 import { SUPERVISION_TYPES } from "../constants";
 import DetailPage from "../detail-page";
 import VizProbationPopulation from "../viz-probation-population";
+import VizSupervisionProgram from "../viz-supervision-program";
 
 const recordIsProbation = (record) =>
   record.supervision_type === SUPERVISION_TYPES.probation;
@@ -29,6 +31,19 @@ const SECTIONS = [
         recordIsProbation
       ),
       locations: judicialDistricts,
+    },
+  },
+  {
+    title: "Free Through Recovery Program",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Vestibulum in finibus tellus, et ullamcorper augue. Quisque eleifend
+    tortor vitae iaculis egestas. Donec dictum, nunc nec tincidunt cursus,
+    ipsum dui gravida.`,
+    VizComponent: VizSupervisionProgram,
+    vizData: {
+      supervisionProgramParticipationByRegion: supervisionProgramParticipationByRegion.filter(
+        recordIsProbation
+      ),
     },
   },
 ];
