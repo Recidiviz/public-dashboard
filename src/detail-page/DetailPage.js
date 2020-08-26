1a3b7d205ccfebbca5e18d489b70a35f082ed79a
Ian MacFarland <ian@recidiviz.org>
2020-08-26 11:31:58 -0700

    increase section spacing

--- src/detail-page/DetailPage.js
+++ src/detail-page/DetailPage.js
@@ -23,6 +23,7 @@ const HeadingContainer = styled.header``;
 const SectionDivider = styled.hr`
   border: none;
   border-top: 1px solid ${(props) => props.theme.colors.divider};
+  margin-top: 88px;
 `;
 
 const DetailSectionContainer = styled.section``;
