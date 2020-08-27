f0a3a0579e25155a450a4999bb027744369e1410
Ian MacFarland <ian@recidiviz.org>
2020-08-27 13:01:52 -0700

    updated 404 styles

--- src/page-not-found/PageNotFound.js
+++ src/page-not-found/PageNotFound.js
@@ -3,24 +3,20 @@ import styled from "styled-components";
 import { HeadingTitle, HeadingDescription } from "../heading";
 
 const Wrapper = styled.div`
-  align-items: center;
-  display: flex;
-  justify-content: center;
   padding: 88px 0;
+  text-align: center;
 `;
 
 export default function PageNotFound() {
   return (
     <Wrapper>
-      <div>
-        <HeadingTitle>Error 404: Page not found.</HeadingTitle>
-        <HeadingDescription>
-          <p>The page that you are looking for is missing.</p>
-          <p>
-            Go to <a href="https://docr.nd.gov">docr.nd.gov</a>
-          </p>
-        </HeadingDescription>
-      </div>
+      <HeadingTitle>Error 404: Page not found.</HeadingTitle>
+      <HeadingDescription>
+        <p>The page that you are looking for is missing.</p>
+        <p>
+          Go to <a href="https://docr.nd.gov">docr.nd.gov</a>
+        </p>
+      </HeadingDescription>
     </Wrapper>
   );
 }
