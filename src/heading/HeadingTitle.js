409a7d657c95099d0f755962727705b6702d7401
Ian MacFarland <ian@recidiviz.org>
2020-08-27 15:04:57 -0700

    smaller secondary nav text

--- src/heading/HeadingTitle.js
+++ src/heading/HeadingTitle.js
@@ -1,5 +1,6 @@
 import { mediaQuery } from "@w11r/use-breakpoint";
 import styled from "styled-components";
+import { THEME } from "../theme";
 
 const HeadingTitle = styled.h1`
   color: ${(props) => props.theme.colors.heading};
@@ -8,14 +9,7 @@ const HeadingTitle = styled.h1`
   margin-top: 0;
   margin-bottom: 16px;
 
-  ${mediaQuery([
-    "mobile-",
-    `
-      font-size: 28px;
-      letter-spacing: -0.015em;
-      line-height: 1;
-    `,
-  ])}
+  ${mediaQuery(["mobile-", THEME.fonts.headingTitleStylesSmall])}
 `;
 
 export default HeadingTitle;
