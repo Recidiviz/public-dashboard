3582168596da725bebb7bc18f55786f06fa32529
Ian MacFarland <ian@recidiviz.org>
2020-09-09 13:03:25 -0700

    increase fade opacity per design review

--- src/utils/highlightFade.js
+++ src/utils/highlightFade.js
@@ -2,6 +2,6 @@ import { color } from "d3-color";
 
 export default function highlightFade(baseColor) {
   const fadedColor = color(baseColor);
-  fadedColor.opacity = 0.3;
+  fadedColor.opacity = 0.45;
   return fadedColor.toString();
 }
