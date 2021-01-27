782f4c86026b6ea42ab5298f4ecaecba885f7099
Ian MacFarland <ian@recidiviz.org>
2021-01-26 16:33:37 -0800

    Statistic component (#298)

--- spotlight-client/src/Statistic/index.ts
+++ spotlight-client/src/Statistic/index.ts
@@ -1,5 +1,5 @@
 // Recidiviz - a data platform for criminal justice reform
-// Copyright (C) 2020 Recidiviz, Inc.
+// Copyright (C) 2021 Recidiviz, Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -15,10 +15,4 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =============================================================================
 
-export { default as animation } from "./animation";
-export { default as breakpoints } from "./breakpoints";
-export { default as Chevron } from "./Chevron";
-export { default as colors } from "./colors";
-export { default as Dropdown } from "./Dropdown";
-export * from "./typography";
-export { default as zIndex } from "./zIndex";
+export { default } from "./Statistic";
