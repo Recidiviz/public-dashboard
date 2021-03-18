a4a6c203733020eb81a9943e72291fbddcde3012
Ian MacFarland <ian@recidiviz.org>
2021-03-18 11:18:46 -0700

    loading spinner (#326)

--- spotlight-client/src/Loading/Loading.tsx
+++ spotlight-client/src/Loading/Loading.tsx
@@ -1,28 +1,44 @@
-// Recidiviz - a data platform for criminal justice reform
-// Copyright (C) 2020 Recidiviz, Inc.
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
-// =============================================================================
-
+import { rem } from "polished";
 import React from "react";
+import styled, { keyframes } from "styled-components/macro";
 
-const Loading: React.FC = () => {
+import { ReactComponent as SpinnerIcon } from "../assets/spinner.svg";
+import { animation, colors } from "../UiLibrary";
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const LoadingWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: ${rem(360)};
+  width: 100%;
+`;
+
+const LoadingSpinnerIcon = styled(SpinnerIcon)`
+  animation: ${rotate} ${animation.defaultDuration}ms linear infinite;
+`;
+
+const LoadingSpinnerText = styled.div`
+  color: ${colors.caption};
+  font-size: ${rem(14)};
+  line-height: 1.5;
+`;
+
+export default function Loading(): JSX.Element {
   return (
-    <div role="status" aria-label="loading">
-      Loading …
-    </div>
+    <LoadingWrapper role="status">
+      <LoadingSpinnerIcon />
+      <LoadingSpinnerText>Data is loading</LoadingSpinnerText>
+    </LoadingWrapper>
   );
-};
-
-export default Loading;
+}
