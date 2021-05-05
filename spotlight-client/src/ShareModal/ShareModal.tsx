79af2b9bc5f1e58d27c9b2b319f0fb0cf917a376
Ian MacFarland <ian@recidiviz.org>
2021-05-05 16:18:01 -0700

    add the buttons

--- spotlight-client/src/ShareModal/ShareModal.tsx
+++ spotlight-client/src/ShareModal/ShareModal.tsx
@@ -18,10 +18,21 @@
 import { observer } from "mobx-react-lite";
 import { rem } from "polished";
 import React, { useState } from "react";
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  TwitterShareButton,
+} from "react-share";
+import { animated, useTransition } from "react-spring/web.cjs";
+import useClipboard from "react-use-clipboard";
 import styled from "styled-components/macro";
+import emailPath from "../assets/email-logo-white.svg";
+import facebookPath from "../assets/facebook-logo-white.svg";
+import twitterPath from "../assets/twitter-logo-white.svg";
 import getUrlForResource from "../routerUtils/getUrlForResource";
 import { useDataStore } from "../StoreProvider";
 import {
+  animation,
   Checkbox,
   colors,
   Modal,
@@ -30,12 +41,40 @@ import {
   typefaces,
 } from "../UiLibrary";
 
+const Button = styled.button.attrs({ type: "button" })`
+  align-items: center;
+  background-color: ${colors.link};
+  border-radius: 3em;
+  border: none;
+  color: ${colors.textLight};
+  cursor: pointer;
+  display: flex;
+  font-size: ${rem(11)};
+  justify-content: center;
+  padding: ${rem(12)} ${rem(16)};
+  transition: background-color ${animation.defaultDuration}ms;
+
+  &:disabled {
+    cursor: not-allowed;
+  }
+
+  &:hover,
+  &:focus {
+    background-color: ${colors.text};
+  }
+
+  &:active {
+    background-color: ${colors.text};
+  }
+`;
+
 const ShareActions = styled.div`
   border-color: ${colors.rule};
   border-style: solid;
   border-width: 1px 0;
   margin: ${rem(24)} 0;
   padding: ${rem(24)} 0;
+  position: relative;
 `;
 
 const UrlText = styled.div`
@@ -47,7 +86,42 @@ const UrlText = styled.div`
   white-space: nowrap;
 `;
 
-const ShareButtons = styled.div``;
+const ShareButtons = styled.div`
+  display: flex;
+
+  & > button {
+    margin: 0 ${rem(4)};
+
+    &:first-of-type {
+      margin-left: 0;
+    }
+
+    &:last-of-type {
+      margin-right: 0;
+    }
+  }
+`;
+
+const IconTwitter = styled.img.attrs({
+  alt: "Twitter",
+  src: twitterPath,
+})``;
+
+const IconEmail = styled.img.attrs({
+  alt: "E-mail",
+  src: emailPath,
+})``;
+
+const IconFacebook = styled.img.attrs({
+  alt: "Facebook",
+  src: facebookPath,
+})``;
+
+const CopyConfirmation = styled(animated.div).attrs({ role: "status" })`
+  bottom: ${rem(4)};
+  font-size: ${rem(11)};
+  position: absolute;
+`;
 
 const IncludeSectionLabel = styled.label`
   align-items: baseline;
@@ -61,7 +135,7 @@ const SectionTitle = styled.span`
 `;
 
 const ShareModal = (
-  props: Omit<SpotlightModalProps, "children">
+  modalProps: Omit<SpotlightModalProps, "children">
 ): JSX.Element => {
   const {
     tenantStore: {
@@ -93,12 +167,47 @@ const ShareModal = (
     }
   }
 
+  const [isCopied, copyUrl] = useClipboard(urlToShare, {
+    successDuration: 5000,
+  });
+
+  const transitionConfirmation = useTransition(isCopied, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
   return (
-    <Modal {...props}>
+    <Modal {...modalProps}>
       <ModalHeading>Share</ModalHeading>
       <ShareActions>
         <UrlText>{urlToShare}</UrlText>
-        <ShareButtons>buttons</ShareButtons>
+        <ShareButtons>
+          <Button onClick={() => copyUrl()}>Copy URL</Button>
+          <EmailShareButton url={urlToShare}>
+            <Button as="div">
+              <IconEmail />
+            </Button>
+          </EmailShareButton>
+          <FacebookShareButton url={urlToShare}>
+            <Button as="div">
+              <IconFacebook />
+            </Button>
+          </FacebookShareButton>
+          <TwitterShareButton url={urlToShare}>
+            <Button as="div">
+              <IconTwitter />
+            </Button>
+          </TwitterShareButton>
+        </ShareButtons>
+        {transitionConfirmation.map(
+          ({ item, key, props }) =>
+            item && (
+              <CopyConfirmation key={key} style={props}>
+                The URL has been copied to your clipboard.
+              </CopyConfirmation>
+            )
+        )}
       </ShareActions>
       <>
         {currentSectionNumber && currentSectionNumber > 1 && (
