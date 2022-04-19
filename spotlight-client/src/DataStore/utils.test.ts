d9b2c143af60fdd3e37de0b861f912f1737c6878
Colin Adams <colin@recidiviz.org>
2022-04-18 18:08:42 -0700

    Add US_ID and US_TN to staging spotlight (#535)
    * start
    
    * Fix up
    
    * lint

--- spotlight-client/src/DataStore/utils.test.ts
+++ spotlight-client/src/DataStore/utils.test.ts
@@ -57,11 +57,17 @@ test("other .gov domains error", () => {
 });
 
 test("staging domains matching tenants", () => {
+  jsdom.reconfigure({ url: "https://us-id.spotlight-staging.recidiviz.org" });
+  expect(getTenantFromDomain()).toBe("US_ID");
+
   jsdom.reconfigure({ url: "https://us-nd.spotlight-staging.recidiviz.org" });
   expect(getTenantFromDomain()).toBe("US_ND");
 
   jsdom.reconfigure({ url: "https://us-pa.spotlight-staging.recidiviz.org" });
   expect(getTenantFromDomain()).toBe("US_PA");
+
+  jsdom.reconfigure({ url: "https://us-tn.spotlight-staging.recidiviz.org" });
+  expect(getTenantFromDomain()).toBe("US_TN");
 });
 
 test("no match", () => {
