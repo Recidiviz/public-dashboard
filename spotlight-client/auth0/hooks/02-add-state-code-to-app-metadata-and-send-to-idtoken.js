a2005b2140f3ae862a657d3eebb1ddf0d53a9142
Terry Tsai <terry@recidiviz.org>
2022-04-29 12:23:14 -0700

    remove tld check

--- spotlight-client/auth0/hooks/02-add-state-code-to-app-metadata-and-send-to-idtoken.js
+++ spotlight-client/auth0/hooks/02-add-state-code-to-app-metadata-and-send-to-idtoken.js
@@ -29,13 +29,11 @@ exports.onExecutePostLogin = async (event, api) => {
     /** 2. Add user's state_code to the app_metadata */
     const domainSplit = userDomain.split(".");
 
-    const tld = domainSplit[domainSplit.length - 1].toLowerCase();
-
     // assumes the state is always the second to last component of the domain
     // e.g. @doc.mo.gov or @nd.gov, but not @nd.docr.gov
     const state = domainSplit[domainSplit.length - 2].toLowerCase();
 
-    if (tld === "gov" && acceptedStateCodes.includes(state)) {
+    if (acceptedStateCodes.includes(state)) {
       const stateCode = `us_${state}`;
       api.user.setAppMetadata("state_code", stateCode);
       api.idToken.setCustomClaim(`${namespace}/app_metadata`, {
